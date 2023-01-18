56e9fc269af352ddfb1749338dfe9e2bc4bd5e32
mr-usely <kimadorna04@gmail.com>
2023-01-19 07:32:36 +0800

    adding summary report

--- src/http/api.js
+++ src/http/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    // baseURL: 'https://agri-commerce-api.onrender.com'
-    baseURL: 'http://localhost:3030'
+    baseURL: 'https://agri-commerce-api.onrender.com'
+    // baseURL: 'http://localhost:3030'
 })
